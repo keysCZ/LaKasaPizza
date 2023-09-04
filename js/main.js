cceabad9c6c31f5f780aa67839b9b9c0aa270f2c
keysCZ <kalia.fh.ks@gmail.com>
2023-09-04 15:19:51 -0400

    demo1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// setTimeout(() => {
+//     import('../assets/datas/pizzas.json', { assert: {type: 'json'} }).then((pizzas) => {
+//         var pizzasDatas = pizzas.default;
+//         return pizzasDatas;
+//     });
+// }, 1000);
+// console.log(pizzas);
+
+import pizzas from "../assets/datas/pizzas.json" assert { type: 'json'};
